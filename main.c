52c3f1e6fba3f52b8e35f423bce9028966fa4c13
vialomur <maxim44m@ukr.net>
2022-02-21 10:45:13 +0100

    01 ZmianametodyPolicz już

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+int Policz(int a,int b);
 
 int main() {
     printf("Hello, World!\n");
+    printf("\n%d", Policz(13,15));
     return 0;
+}
+int Policz(int a,int b)
+{
+    return a+b;
 }
